ea32fbddae7f25c44f9a38f8fda6f9b656b8abdb
KEMISOLA ADENIYI <38558402+khadassah@users.noreply.github.com>
2022-09-06 10:49:52 +0200

    Added some good links

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 #display the output
 print('testing')
+
+
+Links to study more:
+https://www.ibm.com/de-de
+https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/HTMLColorCodes.html
+https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/Programming_Languages.html
+https://towardsdatascience.com/finding-and-removing-duplicate-rows-in-pandas-dataframe-c6117668631f
+https://note.nkmk.me/en/python-pandas-duplicated-drop-duplicates/#:~:text=You%20can%20count%20the%20number,counted%20with%20sum()%20method.&text=If%20you%20want%20to%20count,count%20True%20with%20sum()%20.
+https://linuxhint.com/drop-duplicates-pandas-python/
+https://www.geeksforgeeks.org/get-post-requests-using-python/
+https://pyviz.org/dashboarding/
+https://images.plot.ly/plotly-documentation/images/plotly_js_cheat_sheet.pdf
+https://plotly.com/python/plotly-express
+https://plotly.com/python/graph-objects/
+https://plotly.com/python/getting-started/
